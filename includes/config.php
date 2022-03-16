828c3635e8519bde18cd67c188135f580f34f381
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-03-16 11:12:54 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,3 +1,3 @@
 <?php
- $mysqli_id = mysqli_connect('host', 'epiz_26538064', 'username', 'epiz_26538064_euee');
+ $mysqli_id = mysqli_connect('host', 'username', 'username', 'epiz_26538064_euee');
 ?>
